fee2cab5072c54018fff1a867fde9014ec9827ed
Nick Gabe <nicolasgabrielctt@gmail.com>
2025-03-06 11:23:47 -0300

    chore: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { App } from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 const client = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
       <I18nextProvider i18n={i18n}>
+        <Analytics />
         <App />
       </I18nextProvider>
     </QueryClientProvider>
